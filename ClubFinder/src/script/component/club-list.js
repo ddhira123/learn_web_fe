31e35ac0c65192880c6836edf0dd586d6fc4205a
ddhira123 <dhana_dhira@apps.ipb.ac.id>
2020-08-19 23:34:28 +0700

    Applied ShadowDOM to ClubList and ClubListItem components

--- ClubFinder/src/script/component/club-list.js
+++ ClubFinder/src/script/component/club-list.js
@@ -1,23 +1,65 @@
 import '../component/club-list-item.js'
 
 class ClubList extends HTMLElement{
+    constructor(){
+        super();
+        this.shadowDOM = this.attachShadow({mode: "open"});
+    }
+
     set clubItems(items){
         this._items = items;
         this.render();
     }
 
     render(){
+        this.shadowDOM.innerHTML = `
+        <style>
+        
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        :host {
+            display: block;
+            margin-top: 32px;
+            width: 100%;
+            padding: 16px;
+        }
+        
+        .placeholder{
+            font-weight: lighter;
+           color: rgba(0,0,0,0.5);
+           -webkit-user-select: none;
+           -moz-user-select: none;
+           -ms-user-select: none;
+           user-select: none;
+        }        
+        </style>
+        `;
         this._items.forEach(item => {
             const clubItemElement = document.createElement("club-list-item");
             // memanggil fungsi setter clubItem() pada club-list-item.
             clubItemElement.clubItem = item;
-            this.appendChild(clubItemElement);
+            this.shadowDOM.appendChild(clubItemElement);
         });
     }
 
     renderError(message){
-        this.innerHTML = "";
-        this.innerHTML += `<h2 class="placeholder"> ${message} </h2>`;
+        this.shadowDOM.innerHTML = `
+        <style>
+           .placeholder {
+               font-weight: lighter;
+               color: rgba(0,0,0,0.5);
+               -webkit-user-select: none;
+               -moz-user-select: none;
+               -ms-user-select: none;
+               user-select: none;
+           }
+       </style>
+       `;
+        this.shadowDOM.innerHTML += `<h2 class="placeholder"> ${message} </h2>`;
     }
 }
 customElements.define("club-list", ClubList);
